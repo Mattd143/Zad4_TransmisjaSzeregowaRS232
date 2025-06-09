ebbaf4e91c24b1d8e304d4f8710a00e2a0fc6b89
Mati <Mati@MATI-KOMPUTER>
2025-06-09 21:47:51 +0200

    skonczony projekt

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtEncoded = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.Receive = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(88, 41);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(630, 73);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtEncoded
+            // 
+            this.txtEncoded.Location = new System.Drawing.Point(88, 164);
+            this.txtEncoded.Multiline = true;
+            this.txtEncoded.Name = "txtEncoded";
+            this.txtEncoded.Size = new System.Drawing.Size(630, 97);
+            this.txtEncoded.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(88, 296);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(630, 67);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(88, 381);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(129, 57);
+            this.Send.TabIndex = 3;
+            this.Send.Text = "Wyślij";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click_1);
+            // 
+            // Receive
+            // 
+            this.Receive.Location = new System.Drawing.Point(595, 381);
+            this.Receive.Name = "Receive";
+            this.Receive.Size = new System.Drawing.Size(123, 57);
+            this.Receive.TabIndex = 4;
+            this.Receive.Text = "Odbierz";
+            this.Receive.UseVisualStyleBackColor = true;
+            this.Receive.Click += new System.EventHandler(this.Receive_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(348, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nadajnik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(348, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Odbiornik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(348, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Transmisja";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Receive);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtEncoded);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Symulator transmisji szeregowej RS232";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtEncoded;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Button Receive;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Zad4_TransmisjaSzeregowaRS232
 {
     public partial class Form1 : Form
     {
+        private List<string> profanityList;
+
         public Form1()
         {
             InitializeComponent();
+            LoadProfanityList();
+        }
+
+        private void LoadProfanityList()         // laduje przeklenstwa do listy profanityList
+        {
+            var path = "C:\\Users\\Mati\\source\\repos\\OSK\\Zad4_TransmisjaSzeregowaRS232\\bin\\Debug\\profanityList.txt";
+            profanityList = File.Exists(path) ? File.ReadAllLines(path).ToList() : new List<string>();
+        }
+
+        private string FilterProfanity(string input)           // zamienia przeklenstwa w pliku ProfanityList.txt na gwiazdki
+        {
+            foreach (var badWord in profanityList)
+            {
+                input = input.Replace(badWord, new string('*', badWord.Length));            
+            }
+            return input;
+        }
+
+        private string EncodeToRS232(string input)              
+        {
+            var sb = new StringBuilder();
+            foreach (char c in input)
+            {
+                sb.Append("0"); // start bit
+                string bits = Convert.ToString(c, 2).PadLeft(8, '0');  // Convert.ToString(c, 2) zmienia znak na binarny kod ASCII, .PadLeft(8, '0') zapewnia ze zawsze sa 8 bitow
+                sb.Append(new string(bits.Reverse().ToArray())); // odwraca bity (od LSB do MSB)
+                sb.Append("11"); // two stop bits
+            }
+            return sb.ToString();
+        }
+
+        private string DecodeFromRS232(string bitStream)
+        {
+            var result = new StringBuilder();
+            for (int i = 0; i < bitStream.Length; i += 11)
+            {
+                if (i + 11 > bitStream.Length) break;     // 8 bitow znaku plus bit startu i bity stopu = 11
+                string dataBits = bitStream.Substring(i + 1, 8); // pomija bit startu
+                string normalBits = new string(dataBits.Reverse().ToArray());  // odwraca bity spowrotem od MSB do LSB
+                char c = (char)Convert.ToInt32(normalBits, 2); // zmieniamy na znak ASCII pomijajac dwa pierwsze bity, czyli w tym przypadku bity stopu
+                result.Append(c);
+            }
+            return result.ToString();
+        }
+
+        private void Send_Click_1(object sender, EventArgs e)
+        {
+            string inputText = FilterProfanity(txtInput.Text);   // sprawdza obecnosc przeklenstw
+            string encoded = EncodeToRS232(inputText);  // enkoduje
+            txtEncoded.Text = encoded;
+            // Simulate transmission via file
+            File.WriteAllText("transmission.txt", encoded);  // zapisuje zakodowana transmisje do pliku txt
+        }
+
+        private void Receive_Click_1(object sender, EventArgs e)
+        {
+            if (!File.Exists("transmission.txt")) return;
+            string receivedBits = File.ReadAllText("transmission.txt");    // odczytuje transmisje z pliku
+            string decoded = DecodeFromRS232(receivedBits);  // dekoduje
+            txtOutput.Text = FilterProfanity(decoded);
         }
     }
 }
